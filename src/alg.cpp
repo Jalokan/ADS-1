921f00f7a7bb67df3e23ec5cf1690197a4ae5f1d
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-03-02 21:13:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,48 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+	uint64_t icount = 0;
+	for (uint64_t i = 0; i <= value; i++) {
+		if (value % i) {
+			icount++;
+		}
+	}
+	if (icount == 2) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+	uint64_t icount2 = 0;
+	uint64_t i = 1;
+	while (true) {
+		i++;
+		if (checkPrime(i) == true) {
+			icount2++;
+		}
+		if (icount2 == n) {
+			return i;
+		}
+		
+	}
+
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+	for (uint64_t i = value + 1;; i++) {
+		if (checkPrime(i) == true) {
+			return i;
+		}
+	}
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+	uint64_t sum = 0;
+	for(int i=2;i<hbound;i++)
+		if (checkPrime(i) == true) {
+			sum += i;
+		}
+	return sum;
 }
