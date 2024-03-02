b48fd6aacbc281707d9e2b5a925a82ceb9d34f50
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-03-02 21:19:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,48 +4,48 @@
 
 
 bool checkPrime(uint64_t value) {
-	uint64_t icount = 0;
-	for (uint64_t i = 0; i <= value; i++) {
-		if (value % i) {
-			icount++;
-		}
-	}
-	if (icount == 2) {
-		return true;
-	} else {
-		return false;
-	}
+    uint64_t icount = 0;
+    for (uint64_t i = 0; i <= value; i++) {
+        if (value % i) {
+            icount++;
+        }
+    }
+    if (icount == 2) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t icount2 = 0;
-	uint64_t i = 1;
-	while (true) {
-		i++;
-		if (checkPrime(i) == true) {
-			icount2++;
-		}
-		if (icount2 == n) {
-			return i;
-		}
-		
-	}
+    uint64_t icount2 = 0;
+    uint64_t i = 1;
+    while (true) {
+        i++;
+        if (checkPrime(i) == true) {
+            icount2++;
+        }
+        if (icount2 == n) {
+            return i;
+        }
+        
+    }
 
 }
 
 uint64_t nextPrime(uint64_t value) {
-	for (uint64_t i = value + 1;; i++) {
-		if (checkPrime(i) == true) {
-			return i;
-		}
-	}
+    for (uint64_t i = value + 1;; i++) {
+        if (checkPrime(i) == true) {
+            return i;
+        }
+    }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t sum = 0;
-	for(int i=2;i<hbound;i++)
-		if (checkPrime(i) == true) {
-			sum += i;
-		}
-	return sum;
+    uint64_t sum = 0;
+    for(int i=2;i<hbound;i++)
+        if (checkPrime(i) == true) {
+            sum += i;
+        }
+    return sum;
 }
