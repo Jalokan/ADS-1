3e7c1c206f46551e2800a442569db01be4ed258c
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-03-02 22:26:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -40,6 +40,12 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
+    if (hbound < 3) { 
+        return 0; 
+    }
+    if (hbound == 3) {
+        return 2;
+    }
     uint64_t sum = 0;
     for (int i = 2;i < hbound;i++) {
         if (checkPrime(i) == true) {
