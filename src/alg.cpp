b8fa425b646f0f95ae47fcee3dc57fb765fe5b96
Jalokan <150829310+Jalokan@users.noreply.github.com>
2024-03-02 23:21:48 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,8 +7,7 @@ bool checkPrime(uint64_t value) {
     if (value == 2)  return true;
     if (value % 2 == 0)  return false;
     uint64_t b = value / 2 + 1;
-    for (uint64_t i = 3; i <= b; i = i + 2)
-    {
+    for (uint64_t i = 3; i <= b; i = i + 2) {
         if (value % i == 0)  return false;
     }
     return true;
@@ -40,8 +39,8 @@ uint64_t sumPrime(uint64_t hbound) {
     if (hbound < 3) {
         return 0;
     }
-    if (hbound == 3) { 
-        return 2; 
+    if (hbound == 3) {
+        return 2;
     }
     uint64_t sum = 2;
     for (uint64_t i = 3; i < hbound; i += 2) {
